34a47295c3d372d7ecdd2ca8ef579ea7cf82a6d3
Nathan Hugon <nthugon@gmail.com>
2016-05-13 11:45:55 -0700

    lightbox and multiple breakpoints of column counts added to gallery

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//problem: user goes to a deadend when clicking on image
+//solution: use jquery to create a lightbox overlay of the large image
+
+//create variables for jquery objects
+var $overlay = $('<div id="overlay"></div>');
+var $image = $('<img>');
+
+//add photo to overlay
+$overlay.append($image);
+
+//add overlay to the body
+$("body").append($overlay);
+
+//capture the click event on the link to an image
+$(".gallery a").click(function(event){
+	//prevent click from following link
+	event.preventDefault();
+	//use href of link as src of img in overlay
+	var imageLocation = $(this).attr("href");
+	$image.attr("src", imageLocation);
+	//show overlay
+	$overlay.show();
+});
+
+//hide overlay when clicked
+$overlay.click(function (){
+	$(this).hide();
+});
+
+
+
